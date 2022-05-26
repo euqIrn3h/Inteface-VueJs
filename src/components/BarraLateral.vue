9b359405c2dfe36500a0cd9c740b8221c0b3d81d
euqIrn3h <henrique11rodrigues@gmail.com>
2022-05-26 10:52:14 -0300

    Adicionando os componentes BarraLateral e Formulario - Modificacoes no App

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="Logo">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+header {
+    background-color: black;
+    width: 100%;
+    height: 100vh;
+    color: white;
+    text-align: justify;
+    padding: 1rem;
+}
+
+@media only screen and (max-width: 768px) {
+header {
+    padding: 2.5rem;
+    height: auto;
+}
+}
+
+img {
+    background-color: bisque;
+    width: auto;
+    height: 20%;
+    border: solid;
+    border-color: white;
+    border-radius: 5px;
+
+}
+
+</style>
